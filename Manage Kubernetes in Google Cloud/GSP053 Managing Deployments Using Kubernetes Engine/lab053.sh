7558f30b14c4e34dca0439239b7745a1c67fb4cd
Đỗ Tiến Dũng <htdv.vnptit@gmail.com>
2025-03-07 09:21:57 +0700

    Update lab053.sh

--- Manage Kubernetes in Google Cloud/GSP053 Managing Deployments Using Kubernetes Engine/lab053.sh
+++ Manage Kubernetes in Google Cloud/GSP053 Managing Deployments Using Kubernetes Engine/lab053.sh
@@ -1,3 +1,27 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
 gcloud config set compute/zone $ZONE
 
 gsutil -m cp -r gs://spls/gsp053/orchestrate-with-kubernetes .
@@ -64,3 +88,6 @@ kubectl get pods -o jsonpath --template='{range .items[*]}{.metadata.name}{"\t"}
 kubectl create -f deployments/hello-canary.yaml
 
 kubectl get deployments
+
+
+#-----------------------------------------------------end----------------------------------------------------------#
